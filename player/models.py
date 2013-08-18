a8fba21ecaccb72f6055f5c291596134ccd7677f
aldor <aldor@Aldor.lan>
2013-08-18 10:32:37 +0200

    Change views. improve admin panel, player

--- player/models.py
+++ player/models.py
@@ -1,31 +1,41 @@
 from django.db import models
 from django.contrib import admin
-# Create your models here.
+from django.template.defaultfilters import slugify
+
+
 class Strim(models.Model):
-    lastupdate = models.DateTimeField()
-    lastvisit = models.DateTimeField()
+    lastupdate = models.DateTimeField(auto_now=True)
+    lastvisit = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200)
+    slug = models.SlugField()
+
     def __unicode__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Strim, self).save(*args, **kwargs)
+
 
 class Song(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=300)
     date = models.DateTimeField()
     user = models.CharField(max_length=200)
-    url  = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
     yturl = models.CharField(max_length=200)
     ytid = models.CharField(max_length=60)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
     votes = models.IntegerField(default=0)
     strim = models.ForeignKey(Strim)
+
     def __unicode__(self):
         return self.title
 
+
 class StrimAdmin(admin.ModelAdmin):
-    pass
+    fields = ["name"]
+
 
 class SongAdmin(admin.ModelAdmin):
     pass
